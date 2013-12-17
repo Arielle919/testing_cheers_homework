5bc5698538f18cf30db0175fd1efa12e3512a76b
Arielle Adams <arielle919@gmail.com>
2013-12-17 14:51:42 -0600

    4 test passed(spaces test passed)

--- cheers.rb
+++ cheers.rb
@@ -1,25 +1,18 @@
-# Build on the results of our in-class exploration to output, for example:
-# Give me an... A
-# Give me a... B
-# Give me a... B
-# Give me a... Y
-# ABBY’s just GRAND!
-# When given the input of “Abby”. Note: the “a” vs. “an”
-
-
 puts "What's your name?"
 name = gets.upcase.chomp
 vowels = ['A','E','I','O','U']
+spaces = ' '
 
 name.each_char do |char|
   if vowels.include?(char)
     article = 'an'
     puts 'Give me ' + article +'.. '+char
-  elsif char.nil?
-    puts " "
+  elsif spaces.include?(char)
+    puts ' '
   else
     article = 'a'
     puts 'Give me ' + article +'.. '+char
   end
+
 end
 puts name + "'s " + "just GRAND!"
